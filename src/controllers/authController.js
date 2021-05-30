bafa406ee977cf005611e82cd11ec6c57952d49e
binoy638 <binoy638@gmail.com>
2021-05-30 19:58:49 +0530

    login exception handling

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -11,11 +11,20 @@ exports.registrationController = async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
   const { username, email, password } = req.body;
 
-  const emailExist = await User.findOne({ email });
-  if (emailExist) return res.status(400).send("Email already exisits");
-
-  const usernameExist = await User.findOne({ username });
-  if (usernameExist) return res.status(400).send("Username already exisits");
+  try {
+    const emailExist = await User.findOne({ email });
+    if (emailExist) return res.status(400).send("Email already exisits");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
+  try {
+    const usernameExist = await User.findOne({ username });
+    if (usernameExist) return res.status(400).send("Username already exisits");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
 
   const salt = await bcrypt.genSalt(10);
   const hashedpassword = await bcrypt.hash(password, salt);
@@ -36,21 +45,24 @@ exports.registrationController = async (req, res) => {
 
 exports.loginController = async (req, res) => {
   const { error } = loginValidation(req.body);
-
   if (error) return res.status(400).send(error.details[0].message);
   const { email, password } = req.body;
-  const user = await User.findOne({ email });
-  if (!user) return res.status(401).send("Invalid Credentials");
-
-  const validPass = await bcrypt.compare(password, user.password);
-  if (!validPass) return res.status(401).send("Invalid Credentials");
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(401).send("Invalid Credentials");
+    const validPass = await bcrypt.compare(password, user.password);
+    if (!validPass) return res.status(401).send("Invalid Credentials");
+    const token = jwt.sign(
+      { _id: user._id, username: user.username, email: user.email },
+      process.env.JWT_SECRET_TOKEN
+    );
+    res.status(200).send({ token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server Error");
+  }
 
   //create jwt token
-  const token = jwt.sign(
-    { _id: user._id, username: user.username, email: user.email },
-    process.env.JWT_SECRET_TOKEN
-  );
-  res.status(200).send({ token });
 
   // res.send("Login Success");
 };
